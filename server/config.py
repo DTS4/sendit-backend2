fe478df04bc1b1e9504c53b6be120ad7555955bc
DTS4 <keith.githinji@student.moringaschool.com>
2025-02-21 17:05:58 +0300

    Update config.py with secure secret key and environment variable support

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '87f0472af5773b86bb52188afa26d7885862d752b60ba7c9'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://keith:securepassword123@localhost/parcel_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY') or 'AIzaSyBMSIhZT1MzTsQ3EN-ys3RmndgFO3ygU4w'
