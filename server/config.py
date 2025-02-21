f7dea4135732ecebb217eab4a6e0584303fde4f7
DTS4 <keith.githinji@student.moringaschool.com>
2025-02-21 12:29:44 +0300

    Add a Google maps API key

--- server/config.py
+++ server/config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://keith:securepassword123@localhost/parcel_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY') or 'your-google-maps-api-key'
+    GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY') or 'AIzaSyBMSIhZT1MzTsQ3EN-ys3RmndgFO3ygU4w'
